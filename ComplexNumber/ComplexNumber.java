72c9c27884daa8fcda720322879e19f62475919c
karthik k <116252503+karthik232149@users.noreply.github.com>
2023-06-07 12:59:19 +0530

    Add files via upload

---
+++ ComplexNumber/ComplexNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class ComplexNumber{
+private int real,imaginary;
+ComplexNumber(){}
+ComplexNumber(int real, int imaginary){
+ 
+this.real = real;
+this.imaginary = imaginary;
+}
+ComplexNumber sum(ComplexNumber n1, ComplexNumber n2){
+return new ComplexNumber(n1.real + n2.real, n1.imaginary +n2.imaginary);
+}
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+ComplexNumber n1 = new ComplexNumber();
+ComplexNumber n2 = new ComplexNumber();
+System.out.println("Enter Real part of First Number:");
+n1.real = sc.nextInt();
+System.out.println("Enter Imaginary part of First Number:");
+n1.imaginary = sc.nextInt();
+System.out.println("Enter Real part of Second Number:");
+n2.real = sc.nextInt();
+System.out.println("Enter Imaginary Part of Second number:");
+n2.imaginary = sc.nextInt();
+ComplexNumber sum = n1.sum(n1,n2);
+System.out.println("\nSum:"+ sum.real + "+" +sum.imaginary+"i");
+}
+}
+
